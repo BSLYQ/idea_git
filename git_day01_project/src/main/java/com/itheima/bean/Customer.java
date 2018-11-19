378d6e3c2281e3214490c7a85909a04018f242f1
baishui <bs_lyq@yeah.net>
2018-11-19 16:35:36 +0800

    修改User.java，创建Customer.java

---
+++ git_day01_project/src/main/java/com/itheima/bean/Customer.java
@@ -0,0 +1,5 @@
+package com.itheima.bean;
+
+public class Customer {
+    private Integer id;
+}
--- git_day01_project/src/main/java/com/itheima/bean/User.java
+++ git_day01_project/src/main/java/com/itheima/bean/User.java
@@ -5,4 +5,14 @@ public class User {
     private String name;
     private Integer age;
     private String sex;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
 }
